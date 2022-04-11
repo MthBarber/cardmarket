e347f8ac662c4670ca92289f04db04fc512f97aa
Matt <mthbarber@gmail.com>
2022-04-11 12:31:13 +0100

    Changes to drop down menu, add Header and Footer components to My Order page

--- web/src/components/MyAccount.js
+++ web/src/components/MyAccount.js
@@ -9,56 +9,58 @@ export function MyAccount() {
   }
   return (
     <>
-      {isOpen == false ? (
-        <button
-          id="jost"
-          className="border border-black bg-white rounded-md px-1 mx-2 text-black"
-          onClick={Toggle}
-        >
-          My Account
-        </button>
-      ) : (
-        <div className="flex flex-col items-center">
+      {isAuthenticated ? (
+        isOpen == false ? (
           <button
             id="jost"
             className="border border-black bg-white rounded-md px-1 mx-2 text-black"
             onClick={Toggle}
           >
-            My Account Open
+            My Account
           </button>
-
-          <div className=" bg-gray-100 h-48 w-48 absolute mt-7 rounded-md border-2 border-white">
-            {isAuthenticated ? (
-              <ul className="mr-8 h-full w-full flex flex-col justify-between items-center">
-                <Link to={routes.myOrders()}>
-                  <li className="mt-4">
-                    <button className="border-2 border-gray-600 bg-white rounded-md px-1 mx-2 text-black">
-                      My Orders
+        ) : (
+          <div className="flex flex-col items-center">
+            <button
+              id="jost"
+              className="border border-black bg-white rounded-md font-medium p-1 mx-2 text-black"
+              onClick={Toggle}
+            >
+              My Account
+            </button>
+            {/* Drop down menu here */}
+            <div className=" bg-gray-100 h-48 w-40 absolute mt-9 rounded-md border-2 border-white mr-4">
+              {isAuthenticated ? (
+                <ul className="mr-8 h-full w-full flex flex-col justify-between items-center">
+                  <Link to={routes.myOrders()}>
+                    <li className="mt-4">
+                      <button className="border-2 border-gray-600 bg-white rounded-md px-1 mx-2 text-black">
+                        My Orders
+                      </button>
+                    </li>
+                  </Link>
+                  <li className="flex flex-col items-center mb-4">
+                    <span className="text-black" id="rajdhani-400">
+                      Logged in as {currentUser.username}
+                    </span>{' '}
+                    <button
+                      className="border-2 h-8 border-gray-600 m-2 w-20 rounded-md bg-white text-black md:shadow-md"
+                      type="button"
+                      onClick={logOut}
+                    >
+                      Logout
                     </button>
                   </li>
-                </Link>
-                <li className="flex flex-col items-center mb-4">
-                  <span className="text-black" id="rajdhani-400">
-                    Logged in as {currentUser.username}
-                  </span>{' '}
-                  <button
-                    className="border-2 h-8 border-gray-600 m-2 w-20 rounded-md bg-white text-black md:shadow-md"
-                    type="button"
-                    onClick={logOut}
-                  >
-                    Logout
-                  </button>
-                </li>
-              </ul>
-            ) : (
-              <Link span id="rajdhani-400" to={routes.login()}>
-                <button className="mr-8 h-8 w-16 border-2 border-black rounded-md  bg-white text-black">
-                  Login{' '}
-                </button>
-              </Link>
-            )}
+                </ul>
+              ) : null}
+            </div>
           </div>
-        </div>
+        )
+      ) : (
+        <Link span id="rajdhani-400" to={routes.login()}>
+          <button className="mr-8 h-8 w-16 border-2 border-black rounded-md  bg-white text-black">
+            Login{' '}
+          </button>
+        </Link>
       )}
     </>
   )
--- web/src/pages/MyOrdersPage/MyOrdersPage.js
+++ web/src/pages/MyOrdersPage/MyOrdersPage.js
@@ -6,8 +6,8 @@ import { Footer } from 'src/components/Footer'
 const MyOrdersPage = () => {
   return (
     <div className="w-full h-full flex flex-col">
+      <MetaTags title="My Orders" description="MyOrders page" />
       <Header />
-      <MetaTags title="MyOrders" description="MyOrders page" />
 
       <h1>MyOrdersPage</h1>
       <p>
@@ -17,7 +17,9 @@ const MyOrdersPage = () => {
         My default route is named <code>myOrders</code>, link to me with `
         <Link to={routes.myOrders()}>MyOrders</Link>`
       </p>
-      <Footer />
+      <div className="absolute bottom-0">
+        <Footer />
+      </div>
     </div>
   )
 }
