8090b981f3295258cfde812df13980c45833ab1a
Matt <mthbarber@gmail.com>
2022-04-14 13:04:02 +0100

    Add authorisation to order confirmation page

--- web/src/components/DisplayCompletedOrderCell/DisplayCompletedOrderCell.js
+++ web/src/components/DisplayCompletedOrderCell/DisplayCompletedOrderCell.js
@@ -1,12 +1,19 @@
+import { useAuth } from '@redwoodjs/auth'
+import { Link, routes } from '@redwoodjs/router'
+
 export const QUERY = gql`
   query FindDisplayCompletedOrderQuery($id: Int!) {
     displayCompletedOrder: completedOrder(id: $id) {
       id
+      userId
       orderItems {
         itemForSale {
           price
           card {
             name
+          }
+          user {
+            username
           }
         }
       }
@@ -23,30 +30,65 @@ export const Failure = ({ error }) => (
 )
 
 export const Success = ({ displayCompletedOrder }) => {
+  const { currentUser } = useAuth()
   let keyNum = 0
-  // let sortedArray = []
-  // for (let i = 0; i < displayCompletedOrder.orderItems.length; i++) {
-  //   sortedArray.push(displayCompletedOrder.orderItems[i])
-  // }
-  // console.log(sortedArray)
+  let total = 0
+  displayCompletedOrder.orderItems.map((item) => {
+    total += item.itemForSale.price
+  })
   return (
-    <div className="h-3/4 w-full flex-col border border-gray-600 flex justify-around items-center">
-      <ul className="w-full flex justify-between ">
-        <li className="w-1/3">Item Name</li>
-        <li className="w-1/3">Price</li>
-        <li className="w-1/3">Quantity</li>
-      </ul>
-      <section className="w-full">
-        {displayCompletedOrder.orderItems.map((item) => {
-          return (
-            <ul key={(keyNum += 1)} className=" w-full flex justify-between">
-              <li className="w-1/3">{item.itemForSale.card.name}</li>
-              <li className="w-1/3">£{item.itemForSale.price}</li>
-              <li className="w-1/3"> 1</li>
+    <section className="h-full w-3/4">
+      {currentUser.id === displayCompletedOrder.userId ? (
+        <div className="h-full w-full flex flex-col items-center">
+          <h1 id="rajdhani" className="mt-8 text-2xl">
+            Order Complete!
+          </h1>
+          <h4 id="rajdhani" className="mb-8">
+            Your Order Number: #{displayCompletedOrder.id}
+          </h4>
+          <div
+            id="jost"
+            className="h-3/4 w-3/4 flex-col border border-gray-600 flex"
+          >
+            <ul className="w-full flex justify-around border-b border-gray-600">
+              <li className="w-1/4 text-center">Item Name</li>
+              <li className="w-1/4 text-center">Seller</li>
+              <li className="w-1/4 text-center">Quantity</li>
+              <li className="w-1/4 text-center">Price</li>
             </ul>
-          )
-        })}
-      </section>
-    </div>
+            <section className="w-full">
+              {displayCompletedOrder.orderItems.map((item) => {
+                return (
+                  <ul key={(keyNum += 1)} className=" w-full flex">
+                    <li className="w-1/4 text-center">
+                      {item.itemForSale.card.name}
+                    </li>
+                    <li className="w-1/4 text-center">
+                      {' '}
+                      {item.itemForSale.user.username}{' '}
+                    </li>
+                    <li className="w-1/4 text-center"> 1</li>
+                    <li className="w-1/4 pl-28">
+                      £{item.itemForSale.price.toFixed(2)}
+                    </li>
+                  </ul>
+                )
+              })}
+            </section>
+            <div className="ml-auto mr-24">Total: £{total.toFixed(2)}</div>
+          </div>
+          <p id="rajdhani" className="mt-8 text-lg">
+            Thanks for your order! Hope to see you again soon.
+          </p>
+        </div>
+      ) : (
+        <div className="mt-8 text-xl flex flex-col items-center">
+          <p className="mb-12">You do not have permission to view this.</p>
+          <button className=" w-24 h-8 border border-black rounded-md bg-emerald-500 text-sm text-white">
+            <Link to={routes.home()}>Return home</Link>
+          </button>
+        </div>
+      )}
+    </section>
   )
 }
--- web/src/pages/OrderConfirmationPage/OrderConfirmationPage.js
+++ web/src/pages/OrderConfirmationPage/OrderConfirmationPage.js
@@ -2,23 +2,22 @@ import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import { Header } from 'src/components/Header'
 import { Footer } from 'src/components/Footer'
+
 import DisplayCompletedOrderCell from 'src/components/DisplayCompletedOrderCell/DisplayCompletedOrderCell'
-const OrderConfirmationPage = ({ id }) => {
-  // console.log('id here is', id)
+const OrderConfirmationPage = ({ id, currentUserId }) => {
+  console.log(id, currentUserId)
   return (
-    <div className="h-full w-full flex flex-col">
+    <div className="h-full w-full flex flex-col items-center">
       <MetaTags
         title="Order Confirmation"
         description="OrderConfirmation page"
       />
-      <Header />
-      <section className=" h-full w-3/4 flex flex-col items-center">
-        <h1>Order Complete!</h1>
-        <h4>Your Order Number: #{id}</h4>
-        <div className="w-full">
-          <DisplayCompletedOrderCell id={id} />
-        </div>
-      </section>
+      <div className="w-full">
+        <Header />
+      </div>
+
+      <DisplayCompletedOrderCell id={id} />
+
       <div className="absolute bottom-0">
         <Footer />
       </div>
